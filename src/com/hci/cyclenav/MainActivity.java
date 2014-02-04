7303d705c27d8743abb87f5521fddf9c3f0e936b
Myke Walton <walton.myke@gmail.com>
2014-02-03 18:40:37 -0800

    routing input validation

--- src/com/hci/cyclenav/MainActivity.java
+++ src/com/hci/cyclenav/MainActivity.java
@@ -1,41 +1,50 @@
 package com.hci.cyclenav;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 //import android.app.Activity;
 import android.view.Menu;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import com.mapquest.android.maps.GeoPoint;
 import com.mapquest.android.maps.MapActivity;
 import com.mapquest.android.maps.MapView;
 import com.mapquest.android.maps.MyLocationOverlay;
 import com.mapquest.android.maps.RouteManager;
+import com.mapquest.android.maps.RouteResponse;
+import com.mapquest.android.maps.ServiceResponse.Info;
 
 public class MainActivity extends MapActivity {
 
-	public final static String EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE";
 	protected MapView map;
     private MyLocationOverlay myLocationOverlay;
-    private String myLocation_latLng;
-    private String toLocation;
+    private RouteManager myRoute;
+    private boolean routeOverlay;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
       super.onCreate(savedInstanceState);
       setContentView(R.layout.activity_main);
+      
       setupMapView();
+      setupRoute();
       setupMyLocation();
-      //displayRoute();
     }
     
     public void searchLocation(View view) {
     	EditText editText = (EditText) findViewById(R.id.location_field);
     	String searchText = editText.getText().toString();
     	
-    	toLocation = searchText;
-    	displayRoute();
+    	String from = myLocationOverlay.getMyLocation().getLatitude() + 
+    			"," + myLocationOverlay.getMyLocation().getLongitude();
+    	String to = searchText;
+    	if (isRouteDisplayed()) myRoute.clearRoute();
+    	myRoute.createRoute(from, to);
+    	setDisplayed(true);
     }
 
     // set map
@@ -52,17 +61,14 @@ public class MainActivity extends MapActivity {
         @Override
         public void run() {
           GeoPoint currentLocation = myLocationOverlay.getMyLocation();
-          myLocation_latLng = myLocationOverlay.getMyLocation().getLatitude() + "," + myLocationOverlay.getMyLocation().getLongitude();
           map.getController().animateTo(currentLocation);
           map.getController().setZoom(14);
           map.getOverlays().add(myLocationOverlay);
-          //displayRoute();
           //myLocationOverlay.setFollowing(true);
         }
       });
     }
    
-
     // enable features of the overlay 
     @Override
     protected void onResume() {
@@ -80,16 +86,40 @@ public class MainActivity extends MapActivity {
     }
 
 
-    // create a route and display on the map 
-    private void displayRoute() {
-      RouteManager routeManager = new RouteManager(this);
-      routeManager.setMapView(map);
-      routeManager.createRoute(myLocation_latLng, toLocation);
+    // setup route manager and link to mapview 
+    private void setupRoute() {
+    	String key = getString(R.string.api_key);
+    	myRoute = new RouteManager(this, key);
+        myRoute.setMapView(map);
+        myRoute.setRouteCallback(new RouteManager.RouteCallback() {
+
+			@Override
+			public void onError(RouteResponse routeResponse) {
+				Info info=routeResponse.info;
+				int statusCode=info.statusCode;
+				
+				StringBuilder message =new StringBuilder();
+				message.append("Unable to create route.\n")
+					.append("Error: ").append(statusCode).append("\n")
+					.append("Message: ").append(info.messages);
+				Toast.makeText(getApplicationContext(), message.toString(), Toast.LENGTH_LONG).show();
+			}
+
+			@Override
+			public void onSuccess(RouteResponse routeResponse) {
+				Toast.makeText(getApplicationContext(), "Calculating route", Toast.LENGTH_LONG).show();
+				//STUB
+			}
+		});
     }
 
     @Override
     public boolean isRouteDisplayed() {
-      return true;
+      return routeOverlay;
+    }
+    
+    private void setDisplayed(boolean isDisplayed) {
+    	routeOverlay = isDisplayed;
     }
 
 	@Override
